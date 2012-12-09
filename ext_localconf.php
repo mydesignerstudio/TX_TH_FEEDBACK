e3f14d9c6f5eed4e81c7d7a44b6e4332fdd3d704
Akin Ajewole <dev@typohosting.at>
2012-12-09 14:54:40 +0100

    UPDATE CONF FILES
    - ext_emconf: increase version number to 0.0.1
    - ext_localconf: add "eID" functionality
    - locallang: change contents

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,5 +3,7 @@ if (!defined('TYPO3_MODE')) {
 	die ('Access denied.');
 }
 
+
 t3lib_extMgm::addPItoST43($_EXTKEY, 'pi1/class.tx_thfeedback_pi1.php', '_pi1', 'list_type', 1);
+$TYPO3_CONF_VARS['FE']['eID_include']['sfajax'] = 'EXT:th_feedback/pi1/db_update.php';
 ?>
