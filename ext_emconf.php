4e4d2bd2a0614eaf37754530f6e8099e39864826
Akin Ajewole <dev@typohosting.at>
2012-12-10 14:04:44 +0100

    UPDATE CONF
    - ext_emconf: update version

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "th_feedback".
 #
-# Auto generated 19-11-2012 12:56
+# Auto generated 09-12-2012 16:25
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -30,7 +30,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '0.0.1',
+	'version' => '0.0.2',
 	'constraints' => array(
 		'depends' => array(
 		),
@@ -39,7 +39,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:43:{s:9:"ChangeLog";s:4:"10a7";s:12:"ext_icon.gif";s:4:"eab9";s:17:"ext_localconf.php";s:4:"6da7";s:14:"ext_tables.php";s:4:"30a2";s:13:"locallang.xml";s:4:"d41f";s:16:"locallang_db.xml";s:4:"06f4";s:10:"README.txt";s:4:"eedc";s:13:"th_config.php";s:4:"78a8";s:14:"doc/manual.pdf";s:4:"52de";s:14:"doc/manual.sxw";s:4:"5690";s:14:"doc/manual.txt";s:4:"63eb";s:19:"doc/wizard_form.dat";s:4:"8369";s:20:"doc/wizard_form.html";s:4:"4b34";s:11:"mod1/be.css";s:4:"8be0";s:13:"mod1/conf.php";s:4:"f08c";s:18:"mod1/icon_date.gif";s:4:"ab82";s:14:"mod1/index.php";s:4:"a976";s:18:"mod1/locallang.xml";s:4:"cffe";s:22:"mod1/locallang_mod.xml";s:4:"0c41";s:19:"mod1/moduleicon.gif";s:4:"eab9";s:18:"mod1/poweredby.png";s:4:"5c80";s:14:"pi1/ce_wiz.gif";s:4:"365f";s:31:"pi1/class.tx_thfeedback_pi1.php";s:4:"5fcc";s:39:"pi1/class.tx_thfeedback_pi1_wizicon.php";s:4:"f372";s:13:"pi1/clear.gif";s:4:"cc11";s:17:"pi1/db_update.php";s:4:"def3";s:17:"pi1/locallang.xml";s:4:"4181";s:16:"pi1/css/main.css";s:4:"dc01";s:20:"pi1/css/ui_beige.css";s:4:"96a4";s:19:"pi1/css/ui_blue.css";s:4:"e833";s:23:"pi1/css/ui_custom_1.css";s:4:"d5d9";s:23:"pi1/css/ui_custom_2.css";s:4:"d5d9";s:23:"pi1/css/ui_custom_3.css";s:4:"d5d9";s:20:"pi1/css/ui_green.css";s:4:"c61c";s:19:"pi1/css/ui_pink.css";s:4:"fe80";s:18:"pi1/css/ui_red.css";s:4:"0ee8";s:21:"pi1/css/ui_silver.css";s:4:"ffde";s:20:"pi1/css/ui_white.css";s:4:"96ee";s:27:"pi1/css/ui_whiteclassic.css";s:4:"a8ac";s:25:"pi1/css/ui_whitesolid.css";s:4:"eccf";s:14:"pi1/js/ajax.js";s:4:"fe41";s:16:"pi1/js/jquery.js";s:4:"ddb8";s:33:"pi1/js/jquery_custom_functions.js";s:4:"fb31";}',
+	'_md5_values_when_last_written' => 'a:44:{s:9:"ChangeLog";s:4:"10a7";s:12:"ext_icon.gif";s:4:"eab9";s:17:"ext_localconf.php";s:4:"a30c";s:14:"ext_tables.php";s:4:"30a2";s:14:"ext_tables.sql";s:4:"dc80";s:13:"locallang.xml";s:4:"d41f";s:16:"locallang_db.xml";s:4:"06f4";s:10:"README.txt";s:4:"eedc";s:13:"th_config.php";s:4:"78a8";s:14:"doc/manual.pdf";s:4:"0553";s:14:"doc/manual.sxw";s:4:"76c2";s:14:"doc/manual.txt";s:4:"d41d";s:19:"doc/wizard_form.dat";s:4:"8369";s:20:"doc/wizard_form.html";s:4:"4b34";s:11:"mod1/be.css";s:4:"8be0";s:13:"mod1/conf.php";s:4:"f08c";s:18:"mod1/icon_date.gif";s:4:"ab82";s:14:"mod1/index.php";s:4:"8358";s:18:"mod1/locallang.xml";s:4:"cffe";s:22:"mod1/locallang_mod.xml";s:4:"0c41";s:19:"mod1/moduleicon.gif";s:4:"eab9";s:18:"mod1/poweredby.png";s:4:"5c80";s:14:"pi1/ce_wiz.gif";s:4:"365f";s:31:"pi1/class.tx_thfeedback_pi1.php";s:4:"623d";s:39:"pi1/class.tx_thfeedback_pi1_wizicon.php";s:4:"f372";s:13:"pi1/clear.gif";s:4:"cc11";s:17:"pi1/db_update.php";s:4:"94e5";s:17:"pi1/locallang.xml";s:4:"848b";s:16:"pi1/css/main.css";s:4:"dc01";s:20:"pi1/css/ui_beige.css";s:4:"96a4";s:19:"pi1/css/ui_blue.css";s:4:"e833";s:23:"pi1/css/ui_custom_1.css";s:4:"d5d9";s:23:"pi1/css/ui_custom_2.css";s:4:"d5d9";s:23:"pi1/css/ui_custom_3.css";s:4:"d5d9";s:20:"pi1/css/ui_green.css";s:4:"c61c";s:19:"pi1/css/ui_pink.css";s:4:"fe80";s:18:"pi1/css/ui_red.css";s:4:"0ee8";s:21:"pi1/css/ui_silver.css";s:4:"ffde";s:20:"pi1/css/ui_white.css";s:4:"96ee";s:27:"pi1/css/ui_whiteclassic.css";s:4:"a8ac";s:25:"pi1/css/ui_whitesolid.css";s:4:"eccf";s:14:"pi1/js/ajax.js";s:4:"fe41";s:16:"pi1/js/jquery.js";s:4:"ddb8";s:33:"pi1/js/jquery_custom_functions.js";s:4:"9dca";}',
 	'suggests' => array(
 	),
 );
